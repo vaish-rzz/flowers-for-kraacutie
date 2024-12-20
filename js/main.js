5e594ed373e482746415a3f3e32171ffd120630b
vaish-rzz <vaisakhhhh33@gmail.com>
2024-12-20 15:15:30 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U KIRAACUTIE').split('')
+    const titles = ('I LOVE U KIRAA CUTIE').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
